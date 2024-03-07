06e170920012710af6bc49239aa9a5bf62edae29
zaidkhansahab <kzaid6008@gmail.com>
2024-03-07 22:50:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
